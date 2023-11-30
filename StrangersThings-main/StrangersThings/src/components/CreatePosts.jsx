52dc2412d06480e13a0f74fa9295ae766631de4d
Quinn Jensen <rooner@Quinns-MacBook-Pro.local>
2023-11-30 12:45:14 -0700

    added code

---
+++ StrangersThings-main/StrangersThings/src/components/CreatePosts.jsx
@@ -0,0 +1,58 @@
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import { createPost} from "../api/posts";
+import useAuth from "../hooks/useAuth";
+
+
+export default function NewPost() {
+    const navigate = useNavigate();
+    const {token} = useAuth();
+
+    const [title, setTitle] = useState([]);
+    const [description, setDescription] = useState([]);
+    const [price, setPrice] = useState([]);
+
+    return(
+        <div>
+            <h3> Make Post</h3>
+            <form
+            onSubmit={async (e) => {
+                e.preventDefault();
+                const result = await createPost(title, description,price, token);
+                console.log(result);
+                navigate("/");
+            }} 
+            >
+                <input 
+                type="text"
+                placeholder="title"
+                value="title"
+                onChange={(e) => {
+                    setTitle(e.target.value);
+                }}
+                ></input>
+            </form>
+            <form>
+                <input 
+                type="text"
+                placeholder="description"
+                value="description"
+                onChange={(e) => {
+                    setTitle(e.target.value);
+                }}
+                ></input>
+            </form>
+            <form>
+                <input 
+                type="text"
+                placeholder="price"
+                value="price"
+                onChange={(e) => {
+                    setTitle(e.target.value);
+                }}
+                ></input>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
